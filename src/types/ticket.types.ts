560b51f6695dfeaa1b90a56644d8071e136d99f6
Slavi <gleebslavi@gmail.com>
2023-05-08 17:04:36 +0300

    First commit Created interfaces, schemas, and models for user, ticket and project

---
+++ src/types/ticket.types.ts
@@ -0,0 +1,10 @@
+export interface ITicket {
+    id: string;
+    title: string;
+    projectId: string;
+    assignedTo?: string;
+    description: string;
+    createDate: Date;
+    status: string;
+}
+

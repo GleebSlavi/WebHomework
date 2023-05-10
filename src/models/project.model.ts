a23ccdb1785becf742cc5bc3638c0cd99dfe880c
GleebSlavi <72702346+GleebSlavi@users.noreply.github.com>
2023-05-10 22:08:49 +0300

    Delete src/models directory

--- src/models/project.model.ts
+++
@@ -1,4 +0,0 @@
-import { model } from 'mongoose';
-import { ProjectSchema } from '../schemas/project.schema';
-
-export const ProjectModel = model('Project', ProjectSchema);
--- src/models/ticket.model.ts
+++
@@ -1,4 +0,0 @@
-import { model } from 'mongoose';
-import { TicketSchema } from '../schemas/ticket.schema';
-
-export const TicketModel = model("Ticket", TicketSchema);
